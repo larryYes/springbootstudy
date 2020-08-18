365340003bea824e01469de302d0190531a353d1
刘广进 <liugji@digitalchina.com>
2020-08-18 15:25:24 +0800

    项目配置数据库

--- src/main/java/com/larry/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/larry/springbootstudy/SpringbootstudyApplication.java
@@ -1,8 +1,10 @@
 package com.larry.springbootstudy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.larry.springbootstudy.dao")
 @SpringBootApplication
 public class SpringbootstudyApplication {
 
---
+++ src/main/java/com/larry/springbootstudy/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.larry.springbootstudy.dao;
+
+/**
+ * @author larry
+ * @date 20/8/18 15:25
+ * @description
+ */
+public interface UserDao {
+}
